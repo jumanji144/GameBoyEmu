a5012ec17d6801eba1c1453d019202a365b4acd8
Nowilltolife <55301990+Nowilltolife@users.noreply.github.com>
2021-03-25 21:32:25 +0100

    Updated Registers

--- src/registers.h
+++ src/registers.h
@@ -17,10 +17,10 @@ bool fC;
 
 u16 combine(u8 *reg1, u8 *reg2, u16 *value) {
     if(value) {
-        *reg2 = (u8) (*value >> 8);
-        *reg1 = (u8) (*value);
+        *reg1 = (u8) (*value >> 8);
+        *reg2 = (u8) (*value);
     }else {
-        return (u16) (*reg2 << 8) | *reg1;
+        return (u16) (*reg1 << 8) | *reg2;
     }
 }
 
@@ -38,4 +38,79 @@ u16 DE(u16 *value=NULL) {
 
 u16 HL(u16 *value=NULL) { 
     return combine(&H, &L, value);
+}
+
+void write16(u8 reg, u16 value) {
+    switch(reg) {
+        case 0:
+            B = (u8) (value >> 8);
+            C = (u8)value;
+            break;
+        case 1:
+            D = (u8) (value >> 8);
+            E = (u8)value;
+            break;
+        case 2:
+            H = (u8) (value >> 8);
+            L = (u8)value;
+            break;
+        case 3:
+            SP = value;
+            break;
+    }
+}
+
+void write16(u8 reg, u8 lower, u8 upper) {
+    write16(reg, ((u16)lower) << 8 | ((u16)upper)); 
+}
+
+void write8(u8 reg, u8 value) {
+    switch(reg) {
+        case 0:
+            B = value;
+            break;
+        case 1:
+            D = value;
+            break;
+        case 2:
+            H = value;
+            break;
+        case 3:
+            A = value;
+            break;
+    }
+}
+
+u16 read16(u8 reg) {
+    switch(reg) {
+        case 0:
+            return BC();
+            break;
+        case 1:
+            return DE();
+            break;
+        case 2:
+            return HL();
+            break;
+        case 3:
+            return SP;
+            break;
+    }
+}
+
+u8 read8(u8 reg) {
+    switch(reg) {
+        case 0:
+            return B;
+            break;
+        case 1:
+            return D;
+            break;
+        case 2:
+            return H;
+            break;
+        case 3:
+            return A;
+            break;
+    }
 }
